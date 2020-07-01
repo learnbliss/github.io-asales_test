317c81716e7a1330d89f146511991df7bfa0fe95
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-07-01 08:15:13 +0300

    add icons carrier and fix css

--- src/components/TicketItem/TicketItem.js
+++ src/components/TicketItem/TicketItem.js
@@ -4,11 +4,12 @@ import styles from './TicketItem.module.css'
 import Segments from './Segments';
 
 const TicketItem = ({tickets}) => {
+    console.log('tickets.carrier: ', tickets.carrier);
     return (
         <div className={styles.root}>
             <div className={styles.head}>
                 <span className={styles.price}>{tickets.price} Р</span>
-                <span>{tickets.carrier}</span>
+                <span><img src={`http://pics.avs.io/99/36/${tickets.carrier}.png`} alt={tickets.carrier}/></span>
                 <Segments segments={tickets.segments}/>
             </div>
         </div>
